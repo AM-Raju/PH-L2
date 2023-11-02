6ca2ddbdd6a7f2d91b17f5d6e66dee0ff0bd5233
AM-Raju <mdmormdmorshed.sj2005@gmail.com>
2023-11-03 00:16:17 +0600

    Module 2.4 done

---
+++ 02. Mission 2/2.3/module1/source/2.4.ts
@@ -0,0 +1,36 @@
+// Basic data types
+
+/* string */
+let firstName: string = "Aziz Morshed";
+
+/* Number */
+let roll: number = 123;
+
+/* boolean */
+let isAdmin: boolean = true;
+
+/* undefined */
+let x: undefined = undefined;
+
+/* null */
+let y: null = null;
+
+/* Anytype */
+let d;
+
+d = "Name";
+d = 123;
+d = true;
+
+/* Assign a type on anytype variable. */
+let e: number;
+// Now you can only use number type on this variable.
+
+// Array types
+
+let myName: string[] = ["Md.", "Aziz", "Morshed"];
+
+let eligibleAgeList: number[] = [3, 5];
+// tuple
+let area: [number, number, number] = [5, 10, 12];
+let nameAge: [string, number, boolean] = ["Raju", 36, true];
