169ce7d731d5c4834ec9346afd24013173257189
AM-Raju <mdmormdmorshed.sj2005@gmail.com>
2023-11-06 00:57:04 +0600

    Module 1.12 done

---
+++ 01.Mission1/module1/1.3/source/1.12.ts
@@ -0,0 +1,39 @@
+{
+  // Block starts
+  /* Nullable types */
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+
+  searchName(null);
+
+  /* Unknown type */
+
+  const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = ((value * 1000) / 3600).toFixed(2);
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else if (typeof value === "string") {
+      const [result, unit] = value.split(" ");
+      const convertedSpeed = ((parseFloat(result) * 1000) / 3600).toFixed(2);
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("Wrong input");
+    }
+  };
+
+  getSpeedInMeterPerSecond("1000 kmh^-1");
+
+  /* Never types */
+
+  const throwError = (msg: string): never => {
+    throw new Error(msg);
+  };
+
+  throwError("Error done by mistake");
+  // Block ends
+}
