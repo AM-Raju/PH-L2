f88b5bbf770a0f3cdea4c18fd8dcbe59150b6e2f
AM-Raju <mdmormdmorshed.sj2005@gmail.com>
2023-11-08 01:09:46 +0600

    Module 3.2 done

--- 01.Mission1/module3/3.2.ts
+++ 01.Mission1/module3/3.2.ts
@@ -2,8 +2,8 @@
   // block starts
 
   /* OOP - inheritance */
-
-  class Student {
+  // Parent class
+  class Parent {
     name: string;
     age: number;
     address: string;
@@ -13,6 +13,33 @@
       this.age = age;
       this.address = address;
     }
+    getSleep(numOfHours: number) {
+      console.log(`${this.name} will sleep for ${numOfHours}`);
+    }
   }
+
+  // Class of student
+  class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
+  const student1 = new Student("Raju", 20, "BD");
+
+  // Class of teacher
+  class Teacher extends Parent {
+    designation: string;
+
+    constructor(name: string, age: number, address: string, designation: string) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+
+    takeClass(numOfClass: number) {
+      console.log(`${this.name} will take ${numOfClass}`);
+    }
+  }
+  const teacher1 = new Teacher("Mr. Sohel", 45, "BD", "Assistant Teacher");
+
   // block ends
 }
