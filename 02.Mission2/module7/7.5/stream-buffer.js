3f96e637574a49850d19edfab03d7a49f36b6d06
AM-Raju <mdmormdmorshed.sj2005@gmail.com>
2023-11-18 13:22:06 +0600

    Module 7.5 done

---
+++ 02.Mission2/module7/7.5/stream-buffer.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const { buffer } = require("stream/consumers");
+
+// Creating a server using raw node.js
+const server = http.createServer();
+
+// Listener
+server.on("request", (req, res) => {
+  //   console.log(req);
+  console.log(req.url === "/read-file" && req.method === "GET");
+
+  // streaming file reading
+  const readableStream = fs.createReadStream(process.cwd() + "/text/readf.txt");
+
+  // Streaming buffer listener
+  readableStream.on("data", (buffer) => {
+    res.statusCode = 200;
+    res.write(buffer);
+  });
+
+  // Streaming end listener
+  readableStream.on("end", () => {
+    res.statusCode = 200;
+    res.end("Streaming is over.");
+  });
+
+  // Streaming error listener
+  readableStream.on("error", (error) => {
+    console.log(error);
+    res.statusCode = 500;
+    res.end("Something went wrong");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000.");
+});
