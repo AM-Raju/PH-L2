0949013798dbd7de74d360757ad33ada5b22d722
AM-Raju <mdmormdmorshed.sj2005@gmail.com>
2023-11-20 04:29:23 +0600

    Module 8.3 done partially

--- 02.Mission2/module8/first-project/src/server.ts
+++ 02.Mission2/module8/first-project/src/server.ts
@@ -1,21 +1,16 @@
 import mongoose from "mongoose";
 import app from "./app";
-
-const port = 3000;
+import config from "./config";
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
-
   try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (error) {
     console.log(error);
   }
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 main();
